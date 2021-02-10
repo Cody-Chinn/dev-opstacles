8e5736d01577cc5359748ccd8fe4d01e65062194
Cody Chinn <cchinn1@steelcase.com>
2021-02-10 11:43:40 -0500

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,60 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import { functions } from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLUw1dbBauYVDIG47-rv4TocfYBfEnopY",
+  authDomain: "auth-tutorial-84e90.firebaseapp.com",
+  projectId: "auth-tutorial-84e90",
+  storageBucket: "auth-tutorial-84e90.appspot.com",
+  messagingSenderId: "794251190501",
+  appId: "1:794251190501:web:5335f3ff5ab87e0fc3f40c"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+export const signInWithGoogle = () => {
+  auth.signInWithPopup(provider);
+};
+
+export const generateUserDocument = async (user, additionalData) => {
+  if (!user) return;
+
+  const userRef = firestore.doc(`users/${user.uid}`);
+  const snapshot = await userRef.get();
+
+  if (!snapshot.exists) {
+    const { email, displayName, photoURL } = user;
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        photoURL,
+        ...additionalData
+      });
+    } catch (error) {
+      console.error("Error creating user document", error);
+    }
+  }
+  return getUserDocument(user.uid);
+};
+
+const getUserDocument = async uid => {
+  if (!uid) return null;
+  try {
+    const userDocument = await firestore.doc(`users/${uid}`).get();
+
+    return {
+      uid,
+      ...userDocument.data()
+    };
+  } catch (error) {
+    console.error("Error fetching user", error);
+  }
+};
